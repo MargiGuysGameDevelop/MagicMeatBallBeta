43599198bfa74ce64272c30a25e9554ae6c82d13
newhandfun <newhandfun@gmail.com>
2016-06-26 23:45:28 +0800

    0626 Animation Cilp 90% + autoSetupPrefab 50%
    RT

---
+++ MagicMeatBallBetaUnity/Assets/Scripts/MeatBall/MeatBallConponentSet.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEditor;
+
+#region NetWork
+[RequireComponent(typeof(NetworkIdentity))]
+[RequireComponent(typeof(NetworkTransform))]
+[RequireComponent(typeof(NetworkAnimator))]
+#endregion
+
+#region game
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+#endregion
+
+#region script
+[RequireComponent(typeof(MeatBallMove))]
+[RequireComponent(typeof(MeatBallStatus))]
+[RequireComponent(typeof(Combat))]
+#endregion
+
+public class MeatBallConponentSet : MonoBehaviour {
+
+
+	public void SetUp(){
+		Animator an = GetComponent<Animator> ();
+		an.applyRootMotion = true;
+
+		NetworkIdentity nid = GetComponent<NetworkIdentity> ();
+		nid.localPlayerAuthority = true;
+
+		NetworkTransform ntr = GetComponent<NetworkTransform> ();
+		ntr.transformSyncMode = NetworkTransform.TransformSyncMode.SyncTransform;
+		ntr.syncRotationAxis = NetworkTransform.AxisSyncMode.AxisY;
+
+		NetworkAnimator nAn = GetComponent<NetworkAnimator> ();
+		nAn.animator = an;
+		/*bug
+		if(an.parameterCount != 0)
+		for (int i = 0; i < an.parameterCount; i++) {
+				nAn.SetParameterAutoSend (i,true);
+		}*/
+
+		CapsuleCollider cap = GetComponent<CapsuleCollider> ();
+		cap.center = new Vector3 (0f,0.95f,0f);
+		cap.radius = 0.62f;
+		cap.height = 2f;
+		cap.direction = 1;
+
+		Rigidbody rig = GetComponent<Rigidbody> ();
+		rig.constraints = RigidbodyConstraints.FreezeRotation;
+
+		MeatBallMove move = GetComponent<MeatBallMove> ();
+
+		MeatBallStatus sta = GetComponent<MeatBallStatus> ();
+		sta.damage = 20;
+		sta.MaxHP = 100;
+
+		Combat com = GetComponent<Combat> ();
+	}
+
+}
+
+[CustomEditor(typeof(MeatBallConponentSet))]
+public class MeatBallConponentSetUI :Editor{
+	public override void OnInspectorGUI(){
+		if (GUILayout.Button ("初始化物件")) {
+			((MeatBallConponentSet)target).SetUp();
+		}
+	}
+}
